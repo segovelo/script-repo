89eb687248cb5a1a809dea9be2588a6d1f63f17c
segovelo <gauchosinpingo@gmail.com>
2022-11-06 19:00:05 +0000

    script to check if repository exists locally before creating it

--- scriptrepo.sh
+++ scriptrepo.sh
@@ -9,16 +9,26 @@ do
    read -r -p $'Repository name:' repoName
 done
 
-echo "# $repoName" >> README.md
-echo "Bash script to create README.md file, initialize a .git repository, commit create remote repo and push to https://www.github.com" >> README.md
+if [ ! -d "$repoName" ]
+then
+    echo "Repository doesn't exist. Creating now"
+    mkdir $repoName
+    cd $repoName
 
-git init
-git add .
-git commit -am "Initial commit"
-curl -u $user:$password https://api.github.com/user/repos -d '{"name": "'"$repoName"'", "private":false}'
+    echo "# $repoName" >> README.md
+    echo "Repository created remotely using Bash script https://github.com/segovelo/script-repo.  This script creates README.md file, initialize a .git repository, commit create remote repo and push to https://www.github.com" >> README.md
 
-GIT_URL=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$user/$repoName" | jq -r '.clone_url')
+    git init
+    git add .
+    git commit -am "Initial commit"
+    curl -u $user:$password https://api.github.com/user/repos -d '{"name": "'"$repoName"'", "private":false}'
 
-git branch -M main
-git remote add origin $GIT_URL
-git push -u origin main
+    GIT_URL=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/"$user/$repoName" | jq -r '.clone_url')
+
+    git branch -M main
+    git remote add origin $GIT_URL
+    git push -u origin main
+    echo "Repository $repoName created, finished..."
+else
+    echo "Repository already exists, finished..."
+fi
